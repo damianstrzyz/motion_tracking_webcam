d3304e74bb08ebc34c747264c42fef669434d054
Damian Strzyż <138368043+damianstrzyz@users.noreply.github.com>
2024-02-28 21:43:08 +0100

    Update main.py
    Adding the function of sending an email with captured image.

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import cv2
 import time
+import glob
+import os
+from emailing import send_email
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
+status_list = []
+count = 1
 
+def clean_folder():
+    images = glob.glob("webcam/images/*.png")
+    for image in images:
+        os.remove(image)
 while True:
+    status = 0
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
@@ -24,7 +35,21 @@ while True:
         if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h =cv2.boundingRect(contour)    
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 3)
+        rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 3)
+        if rectangle.any():
+            status = 1
+            cv2.imwrite(f"webcam/images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("webcam/images/*.png")
+            index = int(len(all_images)/2)
+            image_with_object = all_images[index]
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+        clean_folder()
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
